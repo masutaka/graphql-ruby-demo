cd6bd5bfb9bd4d4f7fb60f8eb2d51d4748ffb563
Takashi Masuda <masutaka.net@gmail.com>
2018-05-09 22:39:18 +0900

    Update CreatePostMutation

--- app/graphql/mutations/create_post_mutation.rb
+++ app/graphql/mutations/create_post_mutation.rb
@@ -1,12 +1,19 @@
 Mutations::CreatePostMutation = GraphQL::Relay::Mutation.define do
-  name "CreatePostMutation"
-  # TODO: define return fields
-  # return_field :post, Types::PostType
+  name 'CreatePost'
 
-  # TODO: define arguments
-  # input_field :name, !types.String
+  input_field :subject, !types.String
 
-  resolve ->(obj, args, ctx) {
-    # TODO: define resolve function
+  return_field :post, !Types::PostType
+
+  resolve ->(_obj, inputs, ctx) {
+    begin
+      post = ctx[:current_user].posts.build
+      post.subject = inputs.subject
+      post.save
+    rescue => e
+      return GraphQL::ExecutionError.new(e.message)
+    end
+
+    { post: post }
   }
 end
