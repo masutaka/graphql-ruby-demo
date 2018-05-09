c0f4c21232ab0718401dce741e9f9a671ffa1807
Takashi Masuda <masutaka.net@gmail.com>
2018-05-10 00:07:22 +0900

    InputType

--- app/graphql/mutations/update_address_mutation.rb
+++ app/graphql/mutations/update_address_mutation.rb
@@ -1,16 +1,16 @@
 Mutations::UpdateAddressMutation = GraphQL::Relay::Mutation.define do
   name "UpdateAddressMutation"
 
-  input_field :postal_code, !types.Int
-  input_field :address, !types.String
+  input_field :addressInput, !Types::AddressInputType  # input_fieldにTypes::AddressInputTypeを指定する
 
   return_field :address, !Types::AddressType
 
   resolve ->(_obj, inputs, ctx) {
+    address_input = inputs.addressInput  # input_fieldのネストが一段深くなるので、addressInputを取り出す
     begin
       address = ctx[:current_user].address
-      address.postal_code = inputs.postal_code
-      address.address = inputs.address
+      address.postal_code = address_input.postal_code
+      address.address = address_input.address
       address.save
     rescue => e
       return GraphQL::ExecutionError.new(e.message)
